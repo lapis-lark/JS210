64a208e10d52398692a913af49b4dab14b36afa7
lapis <lapis.santuri@gmail.com>
2023-09-09 16:00:30 -0700

    book exercises

---
+++ book/evenodd.js
@@ -0,0 +1,6 @@
+function evenOrOdd (n) {
+  return ((n % 2) === 0) ? 'even' : 'odd'
+}
+
+console.log(evenOrOdd(5));
+console.log(evenOrOdd(6));
---
+++ book/hello_if.js
@@ -0,0 +1,6 @@
+function capitalize10 (text) {
+  return (text.length > 10 ? text.toUpperCase() : text)
+}
+
+console.log(capitalize10('hello sweetpea'));
+console.log(capitalize10('juice'));
